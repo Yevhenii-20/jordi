38e960d12549fdc2d064a32d8ff7335f600b6bc8
Yevhenii-20 <60322582+Yevhenii-20@users.noreply.github.com>
2024-05-29 19:03:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let popupBg = document.querySelector('.modal');
+let popup = document.querySelector('.modal');
+let openPopupButtons = document.querySelectorAll('.open-popup');
+let closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
+
+
